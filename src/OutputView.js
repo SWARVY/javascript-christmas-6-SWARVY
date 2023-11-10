dcf23cf88ac2133f2e58daa792ffe8d3b9fc7f37
SWARVY <swarvy0826@naver.com>
2023-11-10 18:43:43 +0900

    feat : 콘솔에 내용을 출력하는 Outputview 객체 내부 메서드 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,58 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import {
+  EVENT_OUTPUT_NOTIFICATION_FORMAT,
+  EVENT_OUTPUT_NOTIFICATION_MESSAGE,
+} from './constants/ChristmasEventMessage.js';
+import { PRESENT_EVENT } from './constants/ChristmasEventOption.js';
+
+const OutputView = {
+  print: (message) => Console.print(message),
+
+  printWelcome: () => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.welcome);
+  },
+
+  printOrderMenu: (menuList) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.order);
+    menuList.forEach((menu) =>
+      OutputView.print(EVENT_OUTPUT_NOTIFICATION_FORMAT.orderMenu(menu))
+    );
+  },
+
+  printTotalPriceBeforeEvent: (total) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.totalPriceBeforeEvent);
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_FORMAT.normalPrice(total));
+  },
+
+  printEventItem: () => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.eventItem);
+    OutputView.print(
+      EVENT_OUTPUT_NOTIFICATION_FORMAT.itemAmount(
+        PRESENT_EVENT.name,
+        PRESENT_EVENT.amount
+      )
+    );
+  },
+
+  printAppliedEventList: (appliedEventList) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.appliedEventList);
+    // 추후 객체 설계에 따라 작성
+  },
+
+  printTotalDiscount: (total) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.totalEventDiscount);
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_FORMAT.discountPrice(total));
+  },
+
+  printTotalPriceAfterEvent: (total) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.totalPriceAfterEvent);
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_FORMAT.discountPrice(total));
+  },
+
+  printEventBadge: (badge) => {
+    OutputView.print(EVENT_OUTPUT_NOTIFICATION_MESSAGE.eventBadge);
+    OutputView.print(badge);
+  },
+};
+
+export default OutputView;
