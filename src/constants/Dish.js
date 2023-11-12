cf981acdf28a36a3cfdcac2b602b452fb6b9a54c
SWARVY <swarvy0826@naver.com>
2023-11-12 23:16:01 +0900

    refactor : 자동완성 기능 활성화를 위한 Dish 상수 JSDocs 추가

--- src/constants/Dish.js
+++ src/constants/Dish.js
@@ -1,11 +1,17 @@
 import deepFreeze from '../utils/deepFreeze.js';
 
+/**
+ * @type {{ name: string, price: number }[]}
+ */
 export const APPETIZER = deepFreeze([
   { name: '양송이수프', price: 6_000 },
   { name: '타파스', price: 5_500 },
   { name: '시저샐러드', price: 8_000 },
 ]);
 
+/**
+ * @type {{ name: string, price: number }[]}
+ */
 export const MAIN_DISH = deepFreeze([
   { name: '티본스테이크', price: 55_000 },
   { name: '바비큐립', price: 54_000 },
@@ -13,11 +19,17 @@ export const MAIN_DISH = deepFreeze([
   { name: '크리스마스파스타', price: 25_000 },
 ]);
 
+/**
+ * @type {{ name: string, price: number }[]}
+ */
 export const DESSERT = deepFreeze([
   { name: '초코케이크', price: 15_000 },
   { name: '아이스크림', price: 5_000 },
 ]);
 
+/**
+ * @type {{ name: string, price: number }[]}
+ */
 export const BEVERAGE = deepFreeze([
   { name: '제로콜라', price: 3_000 },
   { name: '레드와인', price: 60_000 },
