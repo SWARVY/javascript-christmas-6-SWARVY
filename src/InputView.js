b069890ce26ef27ae878eec0675e882d19bfc31a
SWARVY <swarvy0826@naver.com>
2023-11-13 22:03:59 +0900

    feat : InputView JSDocs 작성

--- src/InputView.js
+++ src/InputView.js
@@ -2,11 +2,21 @@ import { Console } from '@woowacourse/mission-utils';
 import { EVENT_INPUT_NOTIFICATION_MESSAGE } from './constants/ChristmasEventMessage.js';
 
 const InputView = Object.freeze({
+  /**
+   * @param {string} message - 입력 문구
+   * @returns {string} 입력 데이터
+   */
   readLine: (message) => Console.readLineAsync(message),
 
+  /**
+   * @returns {string} 입력된 방문 날짜
+   */
   readVisitDay: () =>
     InputView.readLine(EVENT_INPUT_NOTIFICATION_MESSAGE.enterVisitDay),
 
+  /**
+   * @returns {string} 입력된 주문 목록
+   */
   readOrder: () =>
     InputView.readLine(EVENT_INPUT_NOTIFICATION_MESSAGE.enterOrder),
 });
