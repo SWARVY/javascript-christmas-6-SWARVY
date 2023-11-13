cadb4ebad968f9d0dfabc390197695c68aba0e9f
SWARVY <swarvy0826@naver.com>
2023-11-13 17:23:04 +0900

    feat : 주문 데이터 에러를 확인하는 OrderValidator 구현

---
+++ src/validator/OrderValidator.js
@@ -0,0 +1,81 @@
+import { EVENT_ERROR_MESSAGE } from '../constants/ChristmasEventMessage.js';
+import { EVENT_OPTION } from '../constants/ChristmasEventOption.js';
+import { DESSERT } from '../constants/Dish.js';
+import ValidationError from '../error/ValidationError.js';
+import deepFreeze from '../utils/deepFreeze.js';
+import getItemInformationByItemName from '../utils/getItemInformationByItemName.js';
+import CommonValidator from './CommonValidator.js';
+
+const OrderValidator = deepFreeze({
+  checkValidOrderMenu(orderList) {
+    orderList.forEach((item) => {
+      const itemInformation = getItemInformationByItemName(item.orderItemName);
+
+      if (!itemInformation) {
+        console.log(1);
+        throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+      }
+    });
+  },
+
+  checkValidOrderAmount(orderList) {
+    orderList.forEach(({ orderItemAmount }) => {
+      if (Number.isNaN(orderItemAmount)) {
+        throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+      }
+
+      if (!Number.isInteger(orderItemAmount)) {
+        throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+      }
+
+      if (orderItemAmount === 0) {
+        throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+      }
+    });
+  },
+
+  checkOrderAmountExceed(orderList) {
+    const totalOrderAmount = orderList.reduce(
+      (acc, { orderItemAmount }) => acc + orderItemAmount,
+      0
+    );
+
+    if (totalOrderAmount > EVENT_OPTION.maximumOrderAmount) {
+      throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+    }
+  },
+
+  checkOrderMenuDuplication(orderList) {
+    const orderMenuList = orderList.map(({ orderItemName }) => orderItemName);
+    const orderMenuSet = new Set(orderMenuList);
+
+    if (orderMenuList.length !== orderMenuSet.size) {
+      throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+    }
+  },
+
+  checkOrderMenuIsOnlyDessert(orderList) {
+    const dessertMenuList = orderList.filter(({ orderItemName }) =>
+      DESSERT.some((menu) => menu.name === orderItemName)
+    );
+
+    if (orderList.length === dessertMenuList.length) {
+      throw new ValidationError(EVENT_ERROR_MESSAGE.invalidOrder);
+    }
+  },
+
+  /**
+   * @param {{ orderItemName: string, orderItemAmount: number }[]} orderList
+   */
+  validateOrder(orderList) {
+    CommonValidator.validateCommon(orderList);
+
+    this.checkValidOrderMenu(orderList);
+    this.checkValidOrderAmount(orderList);
+    this.checkOrderAmountExceed(orderList);
+    this.checkOrderMenuDuplication(orderList);
+    this.checkOrderMenuIsOnlyDessert(orderList);
+  },
+});
+
+export default OrderValidator;
